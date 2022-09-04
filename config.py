aae1910293fc36212f1aaf8aab767460eccf623f
Max Levin <110128097+malevi413@users.noreply.github.com>
2022-09-04 21:06:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5611487245:AAHpJbCGHC4LORZDovEcAS3vWnwJsriQw84'
+
+keys = {'рубль': 'RUB',
+        'евро': 'EUR',
+        'доллар': 'USD',
+        'юань': 'CNY',
+        'франк': 'CHF',
+        'йена': 'JPY',
+        }
